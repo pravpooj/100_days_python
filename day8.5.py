2a51f67cb883263c1b9246b510a89f491b9dc72d
pravpooj <ppoojary@gmail.com>
2022-02-18 20:29:30 -0500

    sdc

--- day8.5.py
+++ day8.5.py
@@ -1,9 +1,9 @@
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+#direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+#text = input("Type your message:\n").lower()
+#shift = int(input("Type the shift number:\n"))
 
 
 #def encrypt(p_text,shift_num):
@@ -39,17 +39,35 @@ shift = int(input("Type the shift number:\n"))
 
 def cipher(direction,text,shift):
     cipher_text = ""
-    for letter in text:
-        position = alphabet.index(letter)
-        if direction == "encode":
-            new_position = position + shift
-        elif direction == "decode":
-            new_position = position - shift
-        new_letter =  alphabet[new_position]
-        cipher_text += new_letter
+    for chr in text:
+        if chr in alphabet:
+            position = alphabet.index(chr)
+            if direction == "encode":
+                new_position = position + shift
+            elif direction == "decode":
+                new_position = position - shift
+            new_letter =  alphabet[new_position]
+            cipher_text += new_letter
+        else:
+            cipher_text += chr
     print(f"your {direction} message is {cipher_text}")
+#cipher(direction,text,shift)
 
-cipher(direction,text,shift)
+t = True
+while t:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    
+    cipher(direction,text,shift)
+
+    I = input("Type 'yes' to continue and 'no' to stop:\n")
+    if I == "no":
+        t = False
+
+
+
+
 
 
 
