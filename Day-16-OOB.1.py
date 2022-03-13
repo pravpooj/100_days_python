ce157bf4223b314608548449cebe1bee4156ff01
pravpooj <ppoojary@gmail.com>
2022-03-13 15:48:12 -0400

    commit
    commit

--- Day-16-OOB.1.py
+++ Day-16-OOB.1.py
@@ -20,6 +20,7 @@ import turtle
 
 from  prettytable import PrettyTable
 
+
 table = PrettyTable()
 table.add_column("Pokemon Name",["Pikachu","Squirtle","Charmander"])
 table.add_column("Type",["Elcetric","Water","Fire"])
